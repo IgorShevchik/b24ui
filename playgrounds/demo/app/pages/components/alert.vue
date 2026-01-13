8391b3ebd5043c2858d393b25175aeab1677db98
AnthonyDavydchyk <62406462+AnthonyDavydchyk@users.noreply.github.com>
2026-01-13 18:25:16 +0300

    refactor(playground): enhance layouts and controls for alert, checkbox-group, calendar

--- playgrounds/demo/app/pages/components/alert.vue
+++ playgrounds/demo/app/pages/components/alert.vue
@@ -1,14 +1,30 @@
 <script setup lang="ts">
 import type { ButtonProps } from '@bitrix24/b24ui-nuxt'
 import theme from '#build/b24ui/alert'
-import ExampleGrid from '../../components/ExampleGrid.vue'
-import ExampleCard from '../../components/ExampleCard.vue'
-import ExampleCardSubTitle from '../../components/ExampleCardSubTitle.vue'
 import SignIcon from '@bitrix24/b24icons-vue/main/SignIcon'
 import MoreMIcon from '@bitrix24/b24icons-vue/outline/MoreMIcon'
 
-const colors = Object.keys(theme.variants.color) as Array<keyof typeof theme.variants.color>
-const sizes = Object.keys(theme.variants.size) as Array<keyof typeof theme.variants.size>
+const colors = Object.keys(theme.variants.color)
+const sizes = Object.keys(theme.variants.size)
+
+const attrs = reactive({
+  color: [theme.defaultVariants.color],
+  size: [theme.defaultVariants.size],
+  inverted: false
+})
+
+const airColors = computed(() => {
+  return colors.filter((color) => {
+    return color.includes('air')
+  })
+})
+
+const data = {
+  title: 'Heads up!',
+  description: 'Let\'s signal the manager that the deal is not moving, the manager does not call the client back and does not respond to his messages. Let\'s assign a task to the manager on behalf of the manager.',
+  icon: SignIcon,
+  close: true
+}
 
 const action = () => [
   {
@@ -59,201 +75,49 @@ const multipleActions = () => [
     }
   }
 ]
-
-const data = {
-  title: 'Heads up!',
-  description: 'Let\'s signal the manager that the deal is not moving, the manager does not call the client back and does not respond to his messages. Let\'s assign a task to the manager on behalf of the manager.',
-  icon: SignIcon,
-  close: true
-}
-
-const oldColors = computed(() => {
-  return colors.filter((color) => {
-    return !color.includes('air')
-  })
-})
-
-const airColors = computed(() => {
-  return colors.filter((color) => {
-    return color.includes('air')
-  })
-})
 </script>
 
 <template>
-  <ExampleGrid v-once class="mb-2">
-    <ExampleCard title="base" class="col-span-4">
-      <ExampleCardSubTitle title="simple" />
-      <div class="mb-4 flex flex-wrap items-center justify-start gap-4">
-        <B24Alert :title="data.title" />
-        <B24Alert :title="data.title" :icon="data.icon" description="example with multiple actions." :actions="multipleActions()" />
-        <B24Alert class="style-filled" :title="data.title" />
-        <B24Alert class="edge-dark style-filled-success" :title="data.title" :icon="data.icon" description="example with multiple actions." :actions="multipleActions()" />
-      </div>
-      <template v-for="size in sizes" :key="size">
-        <ExampleCardSubTitle :title="size as string" />
-        <div class="mb-4 flex flex-wrap items-center justify-start gap-6">
-          <B24Alert
-            :title="data.title"
-            :size="size"
-          />
-          <B24Alert
-            :title="data.title"
-            :description="data.description"
-            :size="size"
-          />
-          <B24Alert
-            :title="data.title"
-            :description="data.description"
-            :icon="data.icon"
-            :close="data.close"
-            :size="size"
-          />
-          <B24Alert
-            :title="data.title"
-            :description="data.description"
-            :avatar="{ src: '/b24ui/demo/avatar/employee.png' }"
-            :close="data.close"
-            :size="size"
-          />
-        </div>
-      </template>
-    </ExampleCard>
-  </ExampleGrid>
-
-  <ExampleGrid v-once class="mb-2">
-    <template v-for="color in airColors" :key="color">
-      <ExampleCard :title="color as string" class="col-span-4">
-        <ExampleCardSubTitle title="sm" />
-        <div class="mb-4 flex flex-wrap items-center justify-start gap-6">
-          <B24Alert
-            :title="data.title"
-            :description="data.description"
-            :icon="data.icon"
-            :close="data.close"
-            :color="color"
-            size="sm"
-          />
-          <B24Alert
-            :title="data.title"
-            :icon="data.icon"
-            :close="data.close"
-            :color="color"
-            :actions="action()"
-            orientation="horizontal"
-            size="sm"
-            inverted
-          />
-          <B24Alert
-            :title="data.title"
-            :description="data.description"
-            :avatar="{ src: '/b24ui/demo/avatar/employee.png' }"
-            :close="data.close"
-            :color="color"
-            :actions="multipleActions()"
-            size="sm"
-          />
-        </div>
-        <ExampleCardSubTitle title="md" />
-        <div class="mb-4 flex flex-wrap items-center justify-start gap-6">
-          <B24Alert
-            :title="data.title"
-            :description="data.description"
-            :icon="data.icon"
-            :close="data.close"
-            :color="color"
-          />
-          <B24Alert
-            :title="data.title"
-            :icon="data.icon"
-            :close="data.close"
-            :color="color"
-            :actions="action()"
-            orientation="horizontal"
-          />
-          <B24Alert
-            :title="data.title"
-            :description="data.description"
-            :icon="data.icon"
-            :close="data.close"
-            :color="color"
-            :actions="multipleActions()"
-            inverted
-          />
-        </div>
-      </ExampleCard>
+  <PlaygroundPage>
+    <template #controls>
+      <B24Select v-model="attrs.color" class="w-44" :items="airColors" placeholder="Color" multiple />
+      <B24Select v-model="attrs.size" class="w-32" :items="sizes" placeholder="Size" multiple />
+      <B24Separator orientation="vertical" class="h-10" />
+      <B24Switch v-model="attrs.inverted" class="w-24" placeholder="Inverted" />
     </template>
-  </ExampleGrid>
-
-  <B24Collapsible class="mb-2">
-    <B24Button
-      color="air-secondary-no-accent"
-      label="Deprecate"
-      use-dropdown
-    />
-
-    <template #content>
-      <ExampleGrid v-once class="mb-2">
-        <template v-for="color in oldColors" :key="color">
-          <ExampleCard :title="color as string" class="col-span-4">
-            <ExampleCardSubTitle title="sm" />
-            <div class="mb-4 flex flex-wrap items-center justify-start gap-6">
-              <B24Alert
-                :title="data.title"
-                :description="data.description"
-                :icon="data.icon"
-                :close="data.close"
-                :color="color"
-                size="sm"
-              />
-              <B24Alert
-                :title="data.title"
-                :icon="data.icon"
-                :close="data.close"
-                :color="color"
-                :actions="action()"
-                orientation="horizontal"
-                size="sm"
-              />
-              <B24Alert
-                :title="data.title"
-                :description="data.description"
-                :avatar="{ src: '/b24ui/demo/avatar/employee.png' }"
-                :close="data.close"
-                :color="color"
-                :actions="multipleActions()"
-                size="sm"
-              />
-            </div>
-            <ExampleCardSubTitle title="md" />
-            <div class="mb-4 flex flex-wrap items-center justify-start gap-6">
-              <B24Alert
-                :title="data.title"
-                :description="data.description"
-                :icon="data.icon"
-                :close="data.close"
-                :color="color"
-              />
-              <B24Alert
-                :title="data.title"
-                :icon="data.icon"
-                :close="data.close"
-                :color="color"
-                :actions="action()"
-                orientation="horizontal"
-              />
-              <B24Alert
-                :title="data.title"
-                :description="data.description"
-                :icon="data.icon"
-                :close="data.close"
-                :color="color"
-                :actions="multipleActions()"
-              />
-            </div>
-          </ExampleCard>
-        </template>
-      </ExampleGrid>
-    </template>
-  </B24Collapsible>
+    <Matrix v-slot="props" :attrs="attrs" container-class="max-w-80">
+      <B24Alert :title="data.title" v-bind="props" />
+      <B24Alert :title="data.title" :icon="data.icon" v-bind="props" />
+      <B24Alert :title="data.title" :icon="data.icon" :close="data.close" v-bind="props" />
+      <B24Alert
+        :title="data.title"
+        :icon="data.icon"
+        :close="data.close"
+        :actions="action()"
+        orientation="horizontal"
+        v-bind="props"
+      />
+      <B24Alert
+        :title="data.title"
+        :icon="data.icon"
+        :close="data.close"
+        :description="data.description"
+        v-bind="props"
+      />
+      <B24Alert
+        :title="data.title"
+        :avatar="{ src: '/b24ui/demo/avatar/employee.png' }"
+        :close="data.close"
+        :description="data.description"
+        v-bind="props"
+      />
+      <B24Alert
+        :title="data.title"
+        :icon="data.icon"
+        description="Example with multiple actions."
+        :actions="multipleActions()"
+        v-bind="props"
+      />
+    </Matrix>
+  </PlaygroundPage>
 </template>
--- playgrounds/demo/app/pages/components/calendar.vue
+++ playgrounds/demo/app/pages/components/calendar.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
 import theme from '#build/b24ui/calendar'
-import { CalendarDate, DateFormatter, getLocalTimeZone } from '@internationalized/date'
+import { CalendarDate } from '@internationalized/date'
 import type { DateValue } from '@internationalized/date'
-import ExampleGrid from '../../components/ExampleGrid.vue'
-import ExampleCard from '../../components/ExampleCard.vue'
-import Calendar1Icon from '@bitrix24/b24icons-vue/main/Calendar1Icon'
 
 const colors = Object.keys(theme.variants.color)
 const sizes = Object.keys(theme.variants.size)
@@ -12,12 +9,14 @@ const sizes = Object.keys(theme.variants.size)
 const attrs = reactive({
   color: [theme.defaultVariants.color],
   size: [theme.defaultVariants.size],
-  multiple: false,
-  range: false
+  disabled: false,
+  numberOfMonths: 1
 })
 
+const multiple = ref(false)
+const range = ref(false)
+
 const value = shallowRef(new CalendarDate(2012, 4, 12))
-const defaultValue = shallowRef(new CalendarDate(2012, 4, 12))
 const valueMultiple = shallowRef([
   new CalendarDate(2012, 4, 12),
   new CalendarDate(2012, 4, 14),
@@ -28,25 +27,6 @@ const valueRange = shallowRef({
   end: new CalendarDate(2012, 4, 21)
 })
 
-const withChipEventsValue = shallowRef(new CalendarDate(2012, 4, 12))
-
-// region With min/max dates ////
-const withMinMaxValue = shallowRef(new CalendarDate(2012, 4, 12))
-const minDate = new CalendarDate(2012, 4, 1)
-const maxDate = new CalendarDate(2012, 4, 30)
-// endregion ////
-
-// region As a DatePicker ////
-const df = new DateFormatter('en-US', {
-  dateStyle: 'medium'
-})
-const datePickerValue = shallowRef(new CalendarDate(2012, 4, 12))
-const datePickerRangeValue = shallowRef({
-  start: new CalendarDate(2012, 4, 12),
-  end: new CalendarDate(2012, 5, 12)
-})
-// endregion ////
-
 const airColors = computed(() => {
   return colors.filter((color) => {
     return color.includes('air')
@@ -55,31 +35,24 @@ const airColors = computed(() => {
 
 // region Helpers ////
 function getColorByDate(date: Date) {
-  const isWeekend = date.getDay() % 6 == 0
-  const isDayMeeting = date.getDay() % 3 == 0
+  const isWeekend = date.getDate() > 5 && date.getDate() < 9
+  const isDayMeeting = date.getDate() == 6
 
   if (isWeekend) {
-    return undefined
+    return 'air-primary-success'
   }
 
   if (isDayMeeting) {
     return 'air-primary-alert'
   }
 
-  return 'air-primary-success'
-}
-const withDisabledDatesValue = shallowRef({
-  start: new CalendarDate(2012, 4, 12),
-  end: new CalendarDate(2012, 4, 14)
-})
-const isDateDisabled = (date: DateValue) => {
-  return date.day >= 15 && date.day <= 17
+  return
 }
 
-const withUnavailableDatesValue = shallowRef({
-  start: new CalendarDate(2012, 4, 12),
-  end: new CalendarDate(2012, 4, 14)
-})
+const isDateDisabled = (date: DateValue) => {
+  return date.day >= 2 && date.day <= 4
+}
+
 const isDateUnavailable = (date: DateValue) => {
   return date.day >= 15 && date.day <= 17
 }
@@ -91,156 +64,28 @@ const isDateUnavailable = (date: DateValue) => {
     <template #controls>
       <B24Select v-model="attrs.color" class="w-44" :items="airColors" placeholder="Color" multiple />
       <B24Select v-model="attrs.size" class="w-32" :items="sizes" placeholder="Size" multiple />
+      <B24InputNumber v-model="attrs.numberOfMonths" class="w-24" placeholder="Number of months" />
       <B24Separator orientation="vertical" class="h-10" />
-      <B24Switch v-model="attrs.multiple" label="Multiple" size="xs" />
-      <B24Switch v-model="attrs.range" label="Range" size="xs" />
+      <B24Switch v-model="multiple" label="Multiple" size="xs" />
+      <B24Switch v-model="range" label="Range" size="xs" />
+      <B24Switch v-model="attrs.disabled" label="Disabled" size="xs" />
     </template>
 
-    <template #default="{ playground }">
-      <Matrix v-slot="props" :attrs="attrs">
-        <B24Calendar v-bind="props" />
-      </Matrix>
-
-      <ExampleGrid v-once>
-        <ExampleCard title="v-model" :use-bg="playground.isUseBg.value">
-          <B24Separator class="my-3" type="dotted" />
-          <div class="mb-4 max-w-[250px] mx-auto">
-            <B24Calendar v-model="value" />
-          </div>
-        </ExampleCard>
-
-        <ExampleCard title="default-value" :use-bg="playground.isUseBg.value">
-          <B24Separator class="my-3" type="dotted" />
-          <div class="mb-4 max-w-[250px] mx-auto">
-            <B24Calendar :default-value="defaultValue" />
-          </div>
-        </ExampleCard>
-
-        <ExampleCard title="as a date picker" :use-bg="playground.isUseBg.value">
-          <B24Separator class="my-3" type="dotted" />
-          <div class="mb-4 flex flex-row flex-wrap items-start justify-start gap-4">
-            <B24Popover :content="{ align: 'start', side: 'bottom' }">
-              <B24Button :icon="Calendar1Icon">
-                <div class="truncate">
-                  {{ datePickerValue ? df.format(datePickerValue.toDate(getLocalTimeZone())) : 'Select a date' }}
-                </div>
-              </B24Button>
-
-              <template #content>
-                <B24Calendar v-model="datePickerValue" class="p-2" />
-              </template>
-            </B24Popover>
-
-            <B24Popover :content="{ align: 'start', side: 'bottom' }">
-              <B24Button :icon="Calendar1Icon">
-                <template v-if="datePickerRangeValue.start">
-                  <div v-if="datePickerRangeValue.end" class="truncate">
-                    {{ df.format(datePickerRangeValue.start.toDate(getLocalTimeZone())) }} - {{ df.format(datePickerRangeValue.end.toDate(getLocalTimeZone())) }}
-                  </div>
-
-                  <div v-else class="truncate">
-                    {{ df.format(datePickerRangeValue.start.toDate(getLocalTimeZone())) }}
-                  </div>
-                </template>
-                <template v-else>
-                  Pick a date
-                </template>
-              </B24Button>
-
-              <template #content>
-                <B24Calendar
-                  v-model="datePickerRangeValue"
-                  class="p-2"
-                  :number-of-months="2"
-                  range
-                />
-              </template>
-            </B24Popover>
-          </div>
-        </ExampleCard>
-
-        <ExampleCard title="multiple" :use-bg="playground.isUseBg.value">
-          <B24Separator class="my-3" type="dotted" />
-          <div class="mb-4 max-w-[250px] mx-auto">
-            <B24Calendar v-model="valueMultiple" multiple />
-          </div>
-        </ExampleCard>
-
-        <ExampleCard title="range" :use-bg="playground.isUseBg.value">
-          <B24Separator class="my-3" type="dotted" />
-          <div class="mb-4 max-w-[250px] mx-auto">
-            <B24Calendar v-model="valueRange" range />
-          </div>
-        </ExampleCard>
-
-        <ExampleCard title="disabled" :use-bg="playground.isUseBg.value">
-          <B24Separator class="my-3" type="dotted" />
-          <div class="mb-4 max-w-[250px] mx-auto">
-            <B24Calendar disabled />
-          </div>
-        </ExampleCard>
-
-        <ExampleCard title="with chip events" :use-bg="playground.isUseBg.value">
-          <B24Separator class="my-3" type="dotted" />
-          <div class="mb-4 max-w-[250px] mx-auto">
-            <B24Calendar v-model="withChipEventsValue">
-              <template #day="{ day }">
-                <B24Chip
-                  :show="!!getColorByDate(day.toDate('UTC'))"
-                  :color="getColorByDate(day.toDate('UTC'))"
-                  size="sm"
-                  :b24ui="{ base: '[--ui-counter-size-sm:6px] px-[2px]' }"
-                >
-                  {{ day.day }}
-                </B24Chip>
-              </template>
-            </B24Calendar>
-          </div>
-        </ExampleCard>
-
-        <ExampleCard title="with disabled dates" :use-bg="playground.isUseBg.value">
-          <B24Separator class="my-3" type="dotted" />
-          <div class="mb-4 max-w-[250px] mx-auto">
-            <B24Calendar
-              v-model="withDisabledDatesValue"
-              :is-date-disabled="isDateDisabled"
-              range
-            />
-          </div>
-        </ExampleCard>
-
-        <ExampleCard title="with unavailable dates" :use-bg="playground.isUseBg.value">
-          <B24Separator class="my-3" type="dotted" />
-          <div class="mb-4 max-w-[250px] mx-auto">
-            <B24Calendar
-              v-model="withUnavailableDatesValue"
-              :is-date-unavailable="isDateUnavailable"
-              range
-            />
-          </div>
-        </ExampleCard>
-
-        <ExampleCard title="with min/max dates" :use-bg="playground.isUseBg.value">
-          <B24Separator class="my-3" type="dotted" />
-          <div class="mb-4 max-w-[250px] mx-auto">
-            <B24Calendar
-              v-model="withMinMaxValue"
-              :min-value="minDate"
-              :max-value="maxDate"
-            />
-          </div>
-        </ExampleCard>
-      </ExampleGrid>
-
-      <B24Separator accent="accent" class="my-4" label="Some months" type="dotted" />
-      <ExampleGrid>
-        <ExampleCard title="numberOfMonths" :use-bg="playground.isUseBg.value" class="sm:col-span-2">
-          <B24Separator class="my-3" type="dotted" />
-          <div class="mb-4 max-w-[500px] mx-auto">
-            <B24Calendar :number-of-months="2" />
-          </div>
-        </ExampleCard>
-      </ExampleGrid>
-    </template>
+    <Matrix v-slot="props" :attrs="attrs">
+      <B24Calendar v-if="range" v-model="valueRange" range :is-date-disabled="isDateDisabled" v-bind="props" />
+      <B24Calendar v-else-if="multiple" v-model="valueMultiple" multiple :is-date-unavailable="isDateUnavailable" v-bind="props" />
+      <B24Calendar v-else v-model="value" v-bind="props">
+        <template #day="{ day }">
+          <B24Chip
+            :show="!!getColorByDate(day.toDate('UTC'))"
+            :color="getColorByDate(day.toDate('UTC'))"
+            size="sm"
+            :b24ui="{ base: '[--ui-counter-size-sm:6px] px-[2px]' }"
+          >
+            {{ day.day }}
+          </B24Chip>
+        </template>
+      </B24Calendar>
+    </Matrix>
   </PlaygroundPage>
 </template>
